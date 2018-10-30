301539cce124370f9bf03a17432c775f5d8e28b6
Jackwhitener <jackwhitener1@gmail.com>
2018-10-30 15:29:38 -0400

    Adds some stuff, making it so the test.. sometimes passes. Not sure how to make it ALWAYS pass, but we'll work on it.

--- random_pairs.rb
+++ random_pairs.rb
@@ -2,13 +2,14 @@ def pair(options)
 	pairs = []
 	deeppair = []
 	count = 0
-	while deeppair.length < 2
-		picked = options.sample
-		deeppair << picked
-		options.delete(picked)
-	end
-	pairs << deeppair
-
+		while deeppair.length < 2
+			picked = options.sample
+			deeppair << picked
+			options.delete(picked)
+		end
+		pairs << deeppair
+		deeppair = []
+	return pairs
 
 
 
