bc220431df06a796bd8334dbcbc4154fa42f9633
Jackwhitener <jackwhitener1@gmail.com>
2018-10-30 15:07:17 -0400

    Testing for a potential pair. With only two options, it won't be too hard to make it pass.

--- random_pairs_test.rb
+++ random_pairs_test.rb
@@ -4,16 +4,19 @@ class My_test < Minitest::Test
 	def test_1
 		assert_equal(1,1)
 	end
-	def test_for_number_of_pairs
+	# def test_for_number_of_pairs
+	# 	options = ["Option One", "Option Two"]
+	# 	assert_equal(1, pair(options))
+	# end
+	# def test_for_more_pairs
+	# 	options = ["Option One", "Option Two", "Option Three", "Option Four"]
+	# 	assert_equal(2, pair(options))
+	# end
+	# def test_for_odd_pairs
+	# 	options = ["Option One", "Option Two", "Option Three", "Option Four", "Option Five"]
+	# 	assert_equal(2, pair(options))
+	end
+	def test_for_one_pair
 		options = ["Option One", "Option Two"]
-		assert_equal(1, pair(options))
-	end
-	def test_for_more_pairs
-		options = ["Option One", "Option Two", "Option Three", "Option Four"]
-		assert_equal(2, pair(options))
-	end
-	def test_for_odd_pairs
-		options = ["Option One", "Option Two", "Option Three", "Option Four", "Option Five"]
-		assert_equal(2, pair(options))
-	end
+		assert_equal([["Option One", "Option Two"]], pair(options))
 end
